6d17646d7cb45d66bca2231f3a74293a98c3b13d
pranav kumar <pranavkumar@pranavs-MacBook-Air.local>
2017-09-06 13:33:38 +0530

    concatenating sub grammars

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,90 @@ var gulp = require('gulp'),
     mkdirp = require('mkdirp'),
     file = require('gulp-file'),
     fs = require('fs'),
+    vinyl = require('vinyl'),
     shell = require('shelljs');
+
+function getDeps(buffer, dirname) {
+    var includereg = /@include.*\n/g;
+    var matches;
+    var repmatches;
+    var depsArr = [];
+    while ((matches = includereg.exec(buffer)) != null) {
+        var matched = matches[0];
+        if (matched) {
+            var filenamereg = /".*"/g;
+            var depPathRel = filenamereg.exec(matched)[0];
+            if (depPathRel) {
+                var temp = depPathRel.replace(/"/g, "");
+                var dep = path.join(dirname, temp);
+                depsArr.push(new vinyl({ cwd: dirname, path: dep }));
+            }
+
+        }
+    }
+
+    var replacereg = /@include.*\n/g;
+
+    var replacestr = [];
+    while (true) {
+        var match = replacereg.exec(buffer);
+        if (match == null) {
+            break;
+        } else {
+            replacestr.push(match[0]);
+        }
+    }
+
+    for (var i = 0; i < replacestr.length; i++) {
+        buffer = buffer.replace(replacestr[i], "");
+    }
+
+    //console.log(buffer);
+    return { buffer: buffer, depsArr: depsArr };
+}
+
+function getBufferAndDeps(file) {
+    var buffer = read.sync(file.path, 'utf-8');
+    var deps = getDeps(buffer, path.dirname(file.path));
+    return { buffer: deps.buffer, deps: deps.depsArr };
+};
 
 gulp.task('watch', function() {
     return watch('grammars/**/*.ne', function(file) {
-        //console.log("changed " + file.path);
-        //console.log("changed cwd" + file.cwd);
+        console.log("changed " + file.path);
         var testname = path.basename(file.path, ".ne") + ".test.ne";
         var output = path.join(path.dirname(file.path), path.basename(file.path, ".ne") + ".js");
-        //console.log(output);
         var compilestr = "nearleyc " + file.path + " -o " + output;
 
-        read(file.path, 'utf-8', function(err, buffer) {
-            //console.log(err);
-            //console.log(typeof buffer);
-            var includereg = /@include.*\n/g;
+        var glob = "start \n";
+        var deps = [];
+        var bufferAndDeps = getBufferAndDeps(file);
+        glob += bufferAndDeps.buffer;
+        deps = deps.concat(bufferAndDeps.deps);
 
-            var matches;
-            while ((matches = includereg.exec(buffer)) != null) {
-                var matched = matches[0];
-                if (matched) {
-                    //console.log(matched);
-                    var filenamereg = /".*"/g;
-                    var depPathRel = filenamereg.exec(matched)[0];
-                    if (depPathRel) {
-                        var temp = depPathRel.replace(/"/g,"");
-                        console.log(path.join(path.dirname(file.path), temp));
-                    }
 
-                }
+
+        while (deps.length >= 0) {
+            //console.log("here");
+            if (deps.length == 0) {
+                console.log(glob);
+                break;
             }
+            var current = deps.pop();
+            //console.log(current);
+            bufferAndDeps = getBufferAndDeps(current);
+            //console.log("here2");
+            //console.log(bufferAndDeps);
+            glob += "\n" + bufferAndDeps.buffer;
+            deps = deps.concat(bufferAndDeps.deps);
+        }
+
+
+
+        /*
+
+        read(file.path, 'utf-8', function(err, buffer) {
+            var depPaths = getDeps(buffer, path.dirname(file.path));
             mkdirp('./tmp', function(err) {
                 if (!err) {
                     fs.writeFile(path.join("./tmp", testname), buffer, function(err) {
@@ -44,6 +98,7 @@ gulp.task('watch', function() {
                 }
             })
         });
+        */
 
         /*
         shell.exec(compilestr, function(code, stdout, stderr) {
